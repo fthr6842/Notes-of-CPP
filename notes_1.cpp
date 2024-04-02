6226cac7c38b5cbd0d1d1da654faf4ac326e0458
6842 Studio <122149669+fthr6842@users.noreply.github.com>
2024-04-02 19:15:18 +0800

    Add files via upload

---
+++ notes_1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+#define day 7
+const int hours = 24;
+
+int main(void)
+{
+
+    printf("There are %d days in a week.\n", day);
+    printf("There are %d hours in a day.\n", hours);
+
+    system("pause");
+    return 0;
+}
